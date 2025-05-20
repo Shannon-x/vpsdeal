f3f37b17f28dc23f295d69fc8c836b4d8abe2443
张书豪 <shuhao54@outlook.com>
2025-05-20 22:07:18 +0800

    修改Contact页面，使用简化版Footer组件，只保留版权信息

---
+++ vps-deals/src/components/SimpleFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="bg-gray-800 text-white py-4">
+    <div class="container mx-auto px-4">
+      <!-- 版权信息 -->
+      <div class="text-center text-gray-300 text-xs">
+        <p>&copy; {{ new Date().getFullYear() }} 优惠VPS推荐. 保留所有权利.</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'SimpleFooter'
+}
+</script> 
--- vps-deals/src/views/Contact.vue
+++ vps-deals/src/views/Contact.vue
@@ -92,7 +92,7 @@
               <svg class="w-5 h-5 text-gray-500 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                       </svg>
-              <span class="text-gray-700">contact@vpsdeal.com</span>
+              <span class="text-gray-700">starorigin2024@gmail.com</span>
                     </div>
             <div class="flex items-center">
               <svg class="w-5 h-5 text-gray-500 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -105,13 +105,13 @@
       </div>
     </main>
     
-    <Footer />
+    <SimpleFooter />
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue';
+import SimpleFooter from '../components/SimpleFooter.vue';
 import { ref } from 'vue';
 import { useStore } from 'vuex';
 
@@ -119,7 +119,7 @@ export default {
   name: 'Contact',
   components: {
     Header,
-    Footer
+    SimpleFooter
   },
   setup() {
     const store = useStore();
